d7381edbeb356e84b370e36787a16697df7aa2a4
xiongyue <499402657@qq.com>
2017-11-15 23:58:28 +0800

    提交Webtest工程

---
+++ src/main/java/com/xy/web/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.xy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(DemoApplication.class);
+	}
+
+}
